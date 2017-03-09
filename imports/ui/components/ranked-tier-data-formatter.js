4860ca7f0e04a0706271f57e306b94a994ad4b98
Fabien Saulier <fsaulier@gmail.com>
2017-03-09 03:33:48 +0100

    up color winrate data formatter

--- imports/ui/components/ranked-tier-data-formatter.js
+++ imports/ui/components/ranked-tier-data-formatter.js
@@ -2,9 +2,21 @@
 import { ListGroup, Alert, Table, Media} from 'react-bootstrap';
 import {TierIconImage} from './tier-icon';
 
+getWinrateColor = (winrate) => {
+  if(winrate < 55)
+    return 'black';
+  else if(winrate < 60)
+    return "#f9a825"; // yellow darken-3
+  else if(winrate < 65)
+    return '#ef6c00';// orange darken-3
+  else
+    return '#d84315';// deep-orange darken-3
+}
+
 export const rankedTierDataFormatter = (league, row) => {
   if(!league)
     league = {'tier': 'unranked'};
+  const winrate = Math.round((league.wins/(league.wins+league.losses))*100);
   return(
       <Media style={{'marginLeft': 'auto', 'marginRight': 'auto', 'width': '150px'}}>
         <Media.Left align="middle" style={{'paddingRight':0}}>
@@ -13,7 +25,7 @@ export const rankedTierDataFormatter = (league, row) => {
         <Media.Right  align="middle" style={{'paddingLeft':'5px'}}>
           { league.leaguePoints >= 0 ? // case of unranked
             <span>{league.tier.capitalizeFirstLetter()} {league.division}  {league.leaguePoints} LP
-              <br /><small> {league.wins} W / {league.losses} L</small>
+              <br /><span  style={{ 'color':getWinrateColor(winrate)}}>{winrate}%</span><small>&nbsp;&nbsp;{league.wins+league.losses} games</small>
             </span>
           : // unranked
             <span>{league.tier.capitalizeFirstLetter()}</span>
