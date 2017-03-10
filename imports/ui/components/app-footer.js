970525b5b598c002ead7bda69cfedc66e93473eb
Fabien Saulier <fsaulier@gmail.com>
2017-03-10 14:57:53 +0100

    property text

--- imports/ui/components/app-footer.js
+++ imports/ui/components/app-footer.js
@@ -7,9 +7,11 @@ const footerStyle = {border:'none'};
 export default  class AppNavigation extends React.Component {
   render() {
     return(
-      <Navbar fixedBottom style={footerStyle}>
+      <Navbar  fixedBottom style={footerStyle}>
         <hr />
         <div>
+          <small>Site in beta. Any suggestions, or bugs are welcome - contact: fsaulier@hotmail.com </small><br />
+          <small style={{fontSize:'x-small'	}}>League Of Communities isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</small>
         </div>
       </Navbar>
   );
